8d3989e933e7194f7dd3b8e5d7cceeb8c08b9dfd
Chekhov Ma <chekhov.ma@nxp.com>
2023-04-25 17:19:03 +0800

    imx93: add back lpuart2
    imx93: add back dts node and mmu region for lpuart2
    
    Signed-off-by: Chekhov Ma <chekhov.ma@nxp.com>

--- soc/arm64/nxp_imx/mimx9/mmu_regions.c
+++ soc/arm64/nxp_imx/mimx9/mmu_regions.c
@@ -35,6 +35,11 @@ static const struct arm_mmu_region mmu_regions[] = {
 			      DT_REG_SIZE(DT_NODELABEL(lpuart1)),
 			      MT_DEVICE_nGnRnE | MT_P_RW_U_NA | MT_NS),
 
+	MMU_REGION_FLAT_ENTRY("UART2",
+			      DT_REG_ADDR(DT_NODELABEL(lpuart2)),
+			      DT_REG_SIZE(DT_NODELABEL(lpuart2)),
+			      MT_DEVICE_nGnRnE | MT_P_RW_U_NA | MT_NS),
+
 	MMU_REGION_FLAT_ENTRY("IOMUXC",
 			      DT_REG_ADDR(DT_NODELABEL(iomuxc)),
 			      DT_REG_SIZE(DT_NODELABEL(iomuxc)),
